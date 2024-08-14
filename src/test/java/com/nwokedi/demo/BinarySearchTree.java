59eed9ed8e822fbf76591cde79ab1b1497fd3f53
Manny-Nwokedi <131998414+Manny-Nwokedi@users.noreply.github.com>
2024-08-13 22:00:19 -0230

    update my code

---
+++ src/test/java/com/nwokedi/demo/BinarySearchTree.java
@@ -0,0 +1,4 @@
+package com.nwokedi.demo;
+
+public class BinarySearchTree {
+}
---
+++ src/test/java/com/nwokedi/demo/TreeService.java
@@ -0,0 +1,11 @@
+package com.nwokedi.demo;
+
+public class TreeService {
+    public Object processNumbers(String s) {
+        return null;
+    }
+
+    public Object getPreviousTrees() {
+        return null;
+    }
+}
